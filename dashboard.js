be0f5f0c5d08b25cfea3acbde4d8d8e1846a1c04
KSWarrior <kswarriorpro518@gmail.com>
2025-03-07 22:55:48 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,9 @@
-
+<script>
+        if (!sessionStorage.getItem("loggedIn")) {
+    window.location.href = "login";
+        }
+        document.getElementById("logout").addEventListener("click", function () {
+            sessionStorage.removeItem("loggedIn");
+            window.location.href = "login";
+        });
+    </script>
